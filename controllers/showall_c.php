534fd03093b70d48ecb84bf30b5f4bcb6998dbbb
enerjiser2 <project@idefclub.ru>
2015-04-17 00:33:20 +0300

    в конструктор передается имя файла конфигурации

--- controllers/showall_c.php
+++ controllers/showall_c.php
@@ -2,6 +2,6 @@
 
 require_once __DIR__ . '/../models/NewsArticle.php';
 
-$model = new NewsArticle(new Db());
+$model = new NewsArticle(new Db(__DIR__ . '/../siteconfig.php'));
 
 require_once __DIR__ . '/../views/news_v.php';

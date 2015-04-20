ce5495a625682169d83193f430dfb803b0ede7b0
enerjiser2 <project@idefclub.ru>
2015-04-20 20:02:39 +0300

    Админ контроолер наследуется от NewsController

---
+++ controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/NewsController.php';
+
+class AdminController
+    extends NewsController
+{
+    public function actionAdd()
+    {
+        $this->addArticle();
+    }
+
+    public function actionDelete()
+    {
+        return false;
+    }
+}
